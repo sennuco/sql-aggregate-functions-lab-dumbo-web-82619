f317cd5cb298ff451b4a7ef4ede34d3ad091fd74
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-09 14:56:56 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,11 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) AS average_student_gpa FROM students"
+  "SELECT AVG(gpa) AS average_student_gpa FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) "
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
